4e1ac93d39d0bea5447a5364c64f45206fb51acf
vsozansky <vsozansky@gmail.com>
2019-08-25 20:49:14 +0300

    Добавлены задачи 19 16-28

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task19.task1916;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +14,53 @@ import java.util.List;
 public class Solution {
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader  reader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+        reader.close();
+
+        BufferedReader bufferedReader1 = new BufferedReader(new FileReader(file1));
+        BufferedReader bufferedReader2 = new BufferedReader(new FileReader(file2));
+
+        String line1 = null;
+        String line2 = null;
+
+        while (bufferedReader1.ready()){
+
+            line1 = bufferedReader1.readLine();
+
+            if(line2 != null ) {
+                if(line1.equals(line2)) {
+                    lines.add(new LineItem(Type.SAME,line1));
+                    line2 = null;
+                    continue;
+                } else {
+                    lines.add(new LineItem(Type.ADDED,line2));
+                    //line2 = null;
+                }
+            }
+
+            line2 = bufferedReader2.readLine();
+            if (line1.equals(line2)) {
+                lines.add(new LineItem(Type.SAME,line1));
+                line2 = null;
+            } else {
+                lines.add(new LineItem(Type.REMOVED,line1));
+            }
+
+        }
+
+        if(line2 != null ) {
+            //System.out.println("" + Type.ADDED + " " + line2);
+            lines.add(new LineItem(Type.ADDED,line2));
+        }
+
+        bufferedReader1.close();
+        bufferedReader2.close();
+
     }
 
 
